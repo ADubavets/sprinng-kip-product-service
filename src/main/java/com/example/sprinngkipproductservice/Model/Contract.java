7912a73b35190a14f6d4ba1cd19cdd06467d8349
ADubavets <d.andrey.f@gmail.com>
2022-12-11 16:17:10 +0300

    2022-12-11

--- src/main/java/com/example/sprinngkipproductservice/Model/Contract.java
+++ src/main/java/com/example/sprinngkipproductservice/Model/Contract.java
@@ -3,15 +3,7 @@ package com.example.sprinngkipproductservice.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 import javax.persistence.*;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.example.sprinngkipproductservice.Controller.ContractController;
 
 import java.sql.Date;
 
@@ -19,28 +11,8 @@ import java.sql.Date;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Table(name = "contracts")
 public class Contract {
-    @PostMapping(value="/save_contract")
-    public String saveContract(ContractController contractController, BindingResult result, HttpServletResponse response) {
-        
-    
-        if (result.hasErrors()) {return "redirect:/add_contract-error";}
-    
-        if (contractController.contractService.getByContractNumber(getNumber()) != null) {
-    
-            return "redirect:/add_contract-error";}
-        else {
-            //Передать id в заголовке ответа
-            Contract newContract = contractController.contractService.save(this);
-            long id = newContract.getId();
-            
-            response.addHeader("id", String.valueOf(id));
-            
-            return "redirect:/sales";
-        }
-    }
 
     @Id
     @Column(name = "id")
